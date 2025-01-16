4963c7d84f0e72c25c8ddbe9ad86238d863bc60e
Logesh <lselvaraj@freewheel.com>
2025-01-16 20:12:52 +0530

    Updated 8.12

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__main__)
 
 @app.route('/')
 def home():
-    return "This is a Test App ran by Local System"
+    return "This is a Test App ran by Local System by Logesh"
 
 if __main__ == '__name__':
     app.run(debug=False,port=6060)

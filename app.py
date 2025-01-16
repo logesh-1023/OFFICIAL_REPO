69f579503cb26a319f613e9939f38b4f00ee4bf7
Logesh <lselvaraj@freewheel.com>
2025-01-16 19:22:36 +0530

    From Local system

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__main__)
 
 @app.route('/')
 def home():
-    return "This is A"
+    return "This is a Test App ran by Local System"
 
 if __main__ == '__name__':
     app.run(debug=False,port=6060)

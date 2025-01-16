802ff852b2045ac09b71e76f16aad23c8b5ead05
logesh-1023 <logeshs91@gmail.com>
2025-01-16 14:58:11 +0000

    Commit System

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__main__)
 
 @app.route('/')
 def home():
-    return "This is a Test App ran by Local System by Logesh"
+    return "This is a Test App ran by Local System by Logesh & Commit System"
 
 if __main__ == '__name__':
     app.run(debug=False,port=6060)

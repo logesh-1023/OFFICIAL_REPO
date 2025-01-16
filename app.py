5ad44573b8627bc2883c702d6296c202911e5fc4
logesh-1023 <logeshs91@gmail.com>
2025-01-16 16:01:30 +0000

    Trial 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask('__name__')
 
 @app.route('/')
 def home():
-    return "This is a Test App ran by Local System by Logesh & Commit System"
+    return "This is a Test App ran by Local System by Logesh & Commit System and This is Trial 2"
 
 if __name__ == '__main__':
     app.run(debug=False,port=6060)
